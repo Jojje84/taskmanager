4e353c29b0985d68326a125a8c07ccb2e2a087bc
Jorge Avila <jorgeavilas@icloud.com>
2025-04-18 12:37:08 +0200

    update

--- taskmanager/src/app/core/services/user.service.ts
+++ taskmanager/src/app/core/services/user.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { User } from '../../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private baseUrl = 'http://localhost:3000/users';
@@ -17,5 +17,9 @@ export class UserService {
 
   getUserById(id: number): Observable<User> {
     return this.http.get<User>(`${this.baseUrl}/${id}`);
+  }
+
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.baseUrl, user);
   }
 }
